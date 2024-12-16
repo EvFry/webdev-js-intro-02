6844315edd44611184d009a9ad47797e2159668f
Evlen Fryling <evlen.fryling@gmail.com>
2024-12-16 17:26:09 -0500

    Assigment part 2

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,34 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const year = 2025;
+let resolution ="Get below 300 pounds";
+let meet = true;
+
 
 function updateYear() {
     // Update this function
-
+yearElement.innerText = year;
 }
 
 function updateResolution() {
     // Update this function 
-
+resolutionElement.innerText = resolution;
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+willMeetResolutionElement.innerText = meet;
 }
 
 function render() {
     // Update this function
-
+updateYear();
+updateResolution();
+updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+render();
 })
