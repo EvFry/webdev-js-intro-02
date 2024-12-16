48364fda9c087cfcf0edb82bb8b44f5d655af898
Evlen Fryling <evlen.fryling@gmail.com>
2024-12-16 17:16:47 -0500

    Assigment part 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 // Prevent us from attempting to use variables
 // that are not declared
+/*
 "use strict"
+const myName ="Evlen";
+console.log(myName)
+console.log(typeof myName)
+const one = 75;
+console.log(one)              
+console.log(typeof one);        
 
+let isMentor = false;            
+console.log(isMentor)           
+console.log(typeof isMentor)   
+*/
